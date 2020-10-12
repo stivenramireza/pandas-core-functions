02e5c6043b2e3135643b629e82701ce29cd55ab3
Stiven Ramírez Arango <sramirez@anutibara.com>
2020-10-11 20:03:17 -0500

    Use a Filter to remove outliers in the data

--- pandas_core_functions.py
+++ pandas_core_functions.py
@@ -1,18 +1,25 @@
 import pandas as pd
 from matplotlib import pyplot as plt
+from logger import logger
 
 
 def main():
     df_temp: pd.DataFrame = pd.read_csv('temperature_yearly.csv')
     df_rain: pd.DataFrame = pd.read_csv('rain_yearly.csv')
 
-    print(df_temp)
-    print(df_rain)
+    logger.info(df_temp)
+    logger.info(df_rain)
 
-    df_temp.plot.scatter(x='Year', y='Temperature', label='Temperature and Year')
+    df_temp_f: pd.DataFrame = df_temp.query('Temperature > 0 & Temperature < 40')
+    logger.info(df_temp_f)
+
+    df_temp_f.plot.scatter(x='Year', y='Temperature', label='Temperature and Year')
     plt.show()
 
-    df_rain.plot.scatter(x='Year', y='Rainfall', label='Rainfall and Year')
+    df_rain_f: pd.DataFrame = df_rain.query('Rainfall > 0 & Rainfall < 6')
+    logger.info(df_rain_f)
+
+    df_rain_f.plot.scatter(x='Year', y='Rainfall', label='Rainfall and Year')
     plt.show()
 
 
