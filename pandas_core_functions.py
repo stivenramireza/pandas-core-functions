92e245a0465aba2a44dcbfa61c0397dc32b40827
Stiven Ramírez Arango <sramirez@anutibara.com>
2020-10-11 20:28:29 -0500

    Combine the data using Pandas Merge

--- pandas_core_functions.py
+++ pandas_core_functions.py
@@ -1,35 +1,51 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 from logger import logger
+from typing import Tuple
 
 
-def filter_data(df_to_filter: pd.DataFrame, query_str: str) -> pd.DataFrame:
-    return df_to_filter.query(query_str)
+def merge_data(df_1: pd.DataFrame, df_2: pd.DataFrame) -> None:
+    df_outer: pd.DataFrame = pd.merge(df_1, df_2, on='Year', how='outer')
+    logger.info(df_outer)
+    df_inner: pd.DataFrame = pd.merge(df_1, df_2, on='Year', how='inner')
+    logger.info(df_inner)
 
 
-def main():
-    # Initial data
-    df_temp: pd.DataFrame = pd.read_csv('temperature_yearly.csv')
-    df_rain: pd.DataFrame = pd.read_csv('rain_yearly.csv')
-
-    logger.info(df_temp)
-    logger.info(df_rain)
-
-    df_temp.plot.scatter(x='Year', y='Temperature', label='Temperature and Year')
-    plt.show()
-    df_rain.plot.scatter(x='Year', y='Rainfall', label='Rainfall and Year')
-    plt.show()
-
-    # Filter data
-    df_temp_f: pd.DataFrame = filter_data(df_temp, 'Temperature > 0 & Temperature < 40')
+def filter_data(df_temp: pd.DataFrame, df_rain: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    df_temp_f: pd.DataFrame = df_temp.query('Temperature > 0 & Temperature < 40')
     logger.info(df_temp_f)
     df_temp_f.plot.scatter(x='Year', y='Temperature', label='Temperature and Year')
     plt.show()
 
-    df_rain_f: pd.DataFrame = filter_data(df_rain, 'Rainfall > 0 & Rainfall < 6')
+    df_rain_f: pd.DataFrame = df_rain.query('Rainfall > 0 & Rainfall < 6')
     logger.info(df_rain_f)
     df_rain_f.plot.scatter(x='Year', y='Rainfall', label='Rainfall and Year')
     plt.show()
+    return df_temp_f, df_rain_f
+
+
+def read_data(dataset_1: str, dataset_2: str) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    df_temp: pd.DataFrame = pd.read_csv(dataset_1)
+    logger.info(df_temp)
+    df_temp.plot.scatter(x='Year', y='Temperature', label='Temperature and Year')
+    plt.show()
+
+    df_rain: pd.DataFrame = pd.read_csv(dataset_2)
+    logger.info(df_rain)
+    df_rain.plot.scatter(x='Year', y='Rainfall', label='Rainfall and Year')
+    plt.show()
+    return df_temp, df_rain
+
+
+def main():
+    # Read data and plot
+    df_temp, df_rain = read_data('temperature_yearly.csv', 'rain_yearly.csv')
+
+    # Filter data and plot
+    df_temp_f, df_rain_f = filter_data(df_temp, df_rain)
+
+    # Merge data
+    merge_data(df_temp_f, df_rain_f)
 
 
 if __name__ == "__main__":
